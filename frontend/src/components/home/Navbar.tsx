e376e36895268197162465f6a6274bdfa05b9114
fekete_oliver <feketeoliver2006@gmail.com>
2026-02-03 22:49:48 +0100

    login form v1

--- frontend/src/components/home/Navbar.tsx
+++ frontend/src/components/home/Navbar.tsx
@@ -1,24 +1,30 @@
+import { Link } from "react-router-dom";
 import chronos_logo from '../../img/logo.png';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-        <div className="trade-name">
-          <h2 className="nav-logo">Chronos </h2>
+      <div className="trade-name">
+        <h2 className="nav-logo">Chronos</h2>
         <img src={chronos_logo} alt="Chronos Logo" />
-        </div>
-        <ul className="nav-links">
-            <li><a href="#features">Funckiók</a></li>
-            <li><a href="#trustlvls">Bizalmi szintek</a></li>
-            <li><a href="#about">Rólunk</a></li>
-            <li><a href="#docs">Dokumentáció</a></li>
-        </ul>
-         <div className="nav-buttons">
-          <button className="btn btn-primary">Bejelentkezés</button>
-          <button className="btn">Regisztrálás</button>
-        </div>
+      </div>
+      <ul className="nav-links">
+        <li><a href="#features">Funkciók</a></li>
+        <li><a href="#trustlvls">Bizalmi szintek</a></li>
+        <li><a href="#about">Rólunk</a></li>
+        <li><a href="#docs">Dokumentáció</a></li>
+      </ul>
+      <div className="nav-buttons">
+        <Link to="/login" className="btn btn-primary">
+          Bejelentkezés
+        </Link>
+        
+        <Link to="/register" className="btn">
+          Regisztrálás
+        </Link>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
