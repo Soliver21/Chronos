4e9a4202f6d30404a8c40ac6e777964a15717feb
fekete.oliver <feketeoliver2006@gmail.com>
2026-02-10 08:30:42 +0100

    Dashboard navigation looks done + working redirect to homepage. Bootstrap icon package implemented and used.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 import App from './App';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 import './index.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
