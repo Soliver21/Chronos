e4cc64079021a6eaf518696f46d7f33842e3c3a3
Roland <roland.bagi007@gmail.com>
2026-03-08 13:28:56 +0100

    Listing transaction fix

--- frontend/src/services/transaction.service.ts
+++ frontend/src/services/transaction.service.ts
@@ -1,7 +1,6 @@
 import { api } from "./api";
 import type { Transaction } from "../types/transaction.types";
 
-// Saját tranzakciók
 export const getMyTransactions = async (): Promise<Transaction[]> => {
     const res = await api.get("/transactions");
     return res.data;
@@ -11,12 +10,23 @@ export const getTransactionById = async (id: number): Promise<Transaction> => {
     const res = await api.get(`/transactions/${id}`);
     return res.data;
 };
+
 export const createTransaction = async (data: {
     listingId: number;
     agreedHours: number;
 }): Promise<Transaction> => {
-    const res = await api.post("/transactions", data);
-    return res.data;
+    const payload = {
+        listingId: String(data.listingId),
+        agreedHours: data.agreedHours,
+    };
+    console.log("Transaction payload:", payload);
+    try {
+        const res = await api.post("/transactions", payload);
+        return res.data;
+    } catch (err: any) {
+        console.log("Backend hiba:", err.response?.data);
+        throw err;
+    }
 };
 
 export const completeTransaction = async (id: number): Promise<Transaction> => {
